e039a04c95852b7d98a6da6813c8017c1fd6fe94
MiracleUkazu <152558180+MiracleUkazu@users.noreply.github.com>
2024-01-15 14:09:14 +0000

    Update read.js

--- public/js/read.js
+++ public/js/read.js
@@ -1,11 +1,32 @@
-let blogId = decodeURI(location.pathname.split('/').pop());
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { doc, setDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-let docRef = db.collection('blogs').doc(blogId);
+await setDoc(doc(db, 'blogs', blogId), data);
 
-docRef.get().then((doc) => {
+const firebaseConfig = {
+    apiKey: "AIzaSyDiO7no4Fr4vgXZPDyhCQewIJA1I-V5O50",
+    authDomain: "blog-hive-d883c.firebaseapp.com",
+    projectId: "blog-hive-d883c",
+    storageBucket: "blog-hive-d883c.appspot.com",
+    messagingSenderId: "3149451026",
+    appId: "1:3149451026:web:92b1fc1975b2d63d2dedcb"
+  };
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+let blogId = decodeURI(location.pathname.split("/").pop());
+
+const myCollection = doc(db, 'blogs');
+
+
+setDoc(myCollection, newData)
+.then((doc) => {
     if (doc.exists) {
         console.log("Document data:", doc.data());
     } else {
         location.replace("/");
     }
+})
+.catch((error) => {
+    console.log("Error getting document:", error);
 })
